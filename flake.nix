5599d46dad3f326a9bddf4e789b2716eaaab4a15
Berkeley True <berkeleytrue@users.noreply.github.com>
2024-08-25 00:16:06 -0700

    feat: initial commit
    Actually builds!!!!!

---
+++ flake.nix
@@ -0,0 +1,76 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+    nixgl.url = "github:nix-community/nixGL";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    boulder.url = "github:berkeleytrue/nix-boulder-banner";
+  };
+
+  outputs = inputs @ {
+    flake-parts,
+    nixpkgs,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.boulder.flakeModule
+      ];
+      systems = ["x86_64-linux"];
+      perSystem = {
+        config,
+        system,
+        ...
+      }: let
+        pkgs = import nixpkgs {
+          inherit system;
+
+          overlays = [
+            inputs.nixgl.overlay
+          ];
+        };
+        run = pkgs.writeShellScriptBin "run" ''
+          cargo run
+        '';
+
+        build = pkgs.writeShellScriptBin "build" ''
+          cargo build
+        '';
+      in {
+        formatter.default = pkgs.alejandra;
+        boulder.commands = [
+          {
+            exec = run;
+            description = "cargo run";
+          }
+          {
+            exec = build;
+            description = "cargo build";
+          }
+        ];
+        devShells.default = pkgs.mkShell {
+          name = "rust";
+          inputsFrom = [
+            config.boulder.devShell
+          ];
+
+          buildInputs = with pkgs; [
+            cargo
+            cargo-generate
+            rustup
+            rustfmt
+            espup
+          ];
+
+          shellHook = ''
+            # espup install --targets esp32s3 --export-file ./exports-esp.sh
+            source ./exports-esp.sh
+            export PATH=$PATH:$HOME/.cargo/bin
+          '';
+        };
+      };
+      flake = {};
+    };
+}
