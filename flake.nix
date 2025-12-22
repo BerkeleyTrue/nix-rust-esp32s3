5e214a27d63227f98291996af258518c0c90a015
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-12-22 00:54:59 -0800

    feat(nix): build web flash binary

--- flake.nix
+++ flake.nix
@@ -28,25 +28,58 @@
             inputs.nixgl.overlay
           ];
         };
+        web-flash = pkgs.rustPlatform.buildRustPackage rec {
+          pname = "web-flash";
+          version = "0.2.1";
+
+          src = pkgs.fetchFromGitHub {
+            owner = "esp-rs";
+            repo = "esp-web-flash-server";
+            rev = "v${version}";
+            hash = "sha256-HUAFAU5oMFB2d59LiJrXYzDhpYDps9Kl1b2SN0x2CrY=";
+          };
+
+          cargoHash = "sha256-5WETh/B/btzxHAylWady1aORmZ9LRRJ41gZmzCe0iUw=";
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+
+          buildInputs = with pkgs; [
+            udev
+            libusb1
+          ];
+
+          meta = with lib; {
+            description = "Web server for flashing ESP devices";
+            homepage = "https://github.com/esp-rs/esp-web-flash-server";
+            license = licenses.mit;
+            mainProgram = "web-flash";
+          };
+        };
       in {
         formatter.default = pkgs.alejandra;
         devShells.default = pkgs.mkShell {
           name = "rust";
 
-          buildInputs = with pkgs; [
-            clippy
-            cargo
-            cargo-generate
-            rustup
-            rustfmt
-            rust-analyzer
-            espup
-            espflash
-            podman
-            libclang.lib
-            ldproxy
-            just
-          ];
+          buildInputs =
+            [
+              web-flash
+            ]
+            ++ (with pkgs; [
+              clippy
+              cargo
+              cargo-generate
+              rustup
+              rustfmt
+              rust-analyzer
+              espup
+              espflash
+              podman
+              libclang.lib
+              ldproxy
+              just
+            ]);
           LD_LIBRARY_PATH = lib.makeLibraryPath [pkgs.stdenv.cc.cc pkgs.libz pkgs.libxml2];
           LIBCLANG_PATH = lib.makeLibraryPath [pkgs.libclang];
 
